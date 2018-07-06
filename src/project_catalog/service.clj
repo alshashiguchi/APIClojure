69b266712927fcc9bdac478cd227070dca3ebcd0
André Hashiguchi <andre.hashiguchi@onclick.com.br>
2018-07-05 23:52:03 -0300

    Using HTTP POST

--- src/project_catalog/service.clj
+++ src/project_catalog/service.clj
@@ -15,6 +15,36 @@
 
             [ring.util.response :as ring-resp]))            
 
+(defn auth0-token []
+  (let [ret
+    (client/post "https://jemez.auth0.com/oauth/token"
+         {:debug false
+          :content-type :json
+          :form-params {:client_id (System/getenv "AUTH0_CLIENT_ID")
+                        :client_secret (System/getenv "AUTH0_SECRET")
+                        :grant_type "client_credentials"}
+                        })
+          ]
+    (json/read-str (ret :body))
+    )
+  )    
+  
+  (defn auth0-connections [tok]
+    (let [ret
+    (client/get "https://jemez.auth0.com/api/connections"
+          {:debug false
+           :content-type :json
+           :accept :json
+           :headers {"Authorization" (format "Bearer %s" tok)}
+           }
+          )
+    ]
+      (ret :body)
+      )
+    )
+  
+    
+
 (defn about-page
   [request]
   (ring-resp/response (format "Clojure %s - served from %s"
